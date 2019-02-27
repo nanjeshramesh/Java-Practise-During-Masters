f78af42d5ca2be2b7d00ed4b8484d8d321b62fc3
Nanjesh Ramesh <nanjeshgowdamr@gmail.com>
2019-02-26 22:58:12 -0800

    Update Java Regex.java

--- Java Regex.java
+++ Java Regex.java
@@ -64,3 +64,15 @@ class MyRegex
             
 }
 
+
+or-------
+    
+    class MyRegex {
+      String pattern;
+    MyRegex() {     
+        String zeroTo255 ="(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+        pattern = zeroTo255 + "." + zeroTo255 + "." + zeroTo255 + "." + zeroTo255;
+
+    }
+}
+
